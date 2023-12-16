3a7b2f55bf9dde760c5958d1682f5468780e4f31
Omar Shemy <OmarShemy@Omar-Shemys-MacBook-Pro.local>
2023-12-16 16:53:06 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('F_KEY')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
